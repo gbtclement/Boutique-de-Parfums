584b9dfcd805f3b4da17553bc39995d23a6236e2
Clément <clementgaubert44@gmail.com>
2025-01-14 09:34:04 +0100

    Modif css + home

--- public/gestion_categ.php
+++ public/gestion_categ.php
@@ -22,47 +22,50 @@ if (isset($_POST['delete'])) {
         foreach ($_POST['categories'] as $categoryId) {
             Categorie::delete($pdo, $categoryId);
         }
-        echo "Les catégories ont été supprimées avec succès.";
-    } else {
-        echo "Aucune catégorie sélectionnée pour la suppression.";
     }
 }
 ?>
 
 <?php include __DIR__ . '/../src/views/header.php'; ?>
 
-<div class="testCateg">
-    <form action="../public/gestion_categ.php" method="POST">
-        <label for="nom">Nom de la catégorie :</label>
-        <input type="text" name="nom" id="nom" required>
+<div class="marginPage">
+    <div class="gestionCateg">
+        <h1>Gestion des catégories</h1>
 
-        <button type="submit" name="submit">Créer la catégorie</button>
-    </form>
+        <div class="creationCateg">
+            <h2>Créer une catégorie</h2>
+            <form action="../public/gestion_categ.php" method="POST">
+                <label for="nom">Nom de la catégorie :</label>
+                <input type="text" name="nom" id="nom" required>
+
+                <button type="submit" name="submit">Créer la catégorie</button>
+            </form>
+        </div>
+
+
+        <div class="listCateg">
+            <h2>Liste des catégories</h2>
+            <form action="../public/gestion_categ.php" method="POST">
+                <table>
+                    <tbody>
+                        <?php
+                        $categories = Categorie::getAll($pdo);
+                        foreach ($categories as $categorie) {
+                            echo "<tr>";
+                            echo "<td><input type='checkbox' name='categories[]' value='" . $categorie->getId() . "'></td>";
+                            echo "<td>" . htmlspecialchars($categorie->getNom()) . "</td>";
+                            echo "</tr>";
+                        }
+                        ?>
+                    </tbody>
+                </table>
+
+                <button type="submit" name="delete">
+                    <img src="../assets/image/trash.svg" alt="Supprimer" style="width: 24px; height: 24px; display:flex;">
+                </button>
+            </form>
+        </div>
+    </div>
 </div>
-
-<h2>Liste des catégories</h2>
-<form action="../public/gestion_categ.php" method="POST">
-    <table>
-        <thead>
-            <tr>
-                <th>Choisir</th>
-                <th>Nom de la catégorie</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php
-            $categories = Categorie::getAll($pdo);
-            foreach ($categories as $categorie) {
-                echo "<tr>";
-                echo "<td><input type='checkbox' name='categories[]' value='" . $categorie->getId() . "'></td>";
-                echo "<td>" . htmlspecialchars($categorie->getNom()) . "</td>";
-                echo "</tr>";
-            }
-            ?>
-        </tbody>
-    </table>
-
-    <button type="submit" name="delete">Supprimer les catégories sélectionnées</button>
-</form>
 
 <?php include __DIR__ . '/../src/views/footer.php'; ?>
--- src/views/header.php
+++ src/views/header.php
@@ -1,23 +1,24 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
     <title>Boutique de Parfums</title>
 </head>
+
 <body>
-<header>
-    <div class="menu">
-        <div class="navigation" id="navigation">
-            <a href="xxx.php">xxx</a>
-            <a href="xxx.php">xxx</a>
-            <a href="xxx.php">xxx</a>
+    <header>
+        <div class="menu">
+            <div class="navigation" id="navigation">
+                <a href="../accueil.php">Accueil</a>
+                <a href="../public/catalogue.php">Catalogue</a>
+                <a href="../public/compte.php">Connexion</a>
+
+            </div>
         </div>
-    </div>
-</header>
+    </header>
 </body>
+
 </html>
-
-
-<script src="/assets/js/script.js"></script>
