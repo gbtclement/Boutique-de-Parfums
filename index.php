84284c22664d09cf4b8ec681e9c0cdf8e6ede2b3
triafus <morgan.lucas27110@gmail.com>
2025-01-14 18:08:51 +0100

    test mvc en cour

--- index.php
+++ index.php
@@ -1,4 +1,31 @@
 <?php
 // Redirige vers accueil.php
 header("Location: accueil.php");
-exit();
+exit(); ?>
+ <?php
+/*
+require_once __DIR__ . '/src/controller/gestionCategorieController.php';
+
+$action = $_GET['action'] ?? 'list';
+
+// Appelle la méthode correspondante dans le contrôleur
+switch ($action) {
+    case 'list':
+        listCategories();
+        break;
+    case 'create':
+        createCategory();
+        break;
+    case 'update':
+        updateCategory();
+        break;
+    case 'delete':
+        deleteCategories();
+        break;
+    default:
+        echo "Action inconnue.";
+        break;
+}
+*/
+?>
+
